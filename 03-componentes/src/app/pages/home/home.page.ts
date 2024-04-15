454c650d0aae11ed9ee784260291e365f4b6fa19
4hiam <ambeniitez.home@gmail.com>
2024-04-15 02:52:10 -0300

    up

---
+++ 03-componentes/src/app/pages/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirecTo: '/alert'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
